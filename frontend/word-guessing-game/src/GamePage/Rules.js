aefc766ded2261862819ae11aa6c3c176a72de1e
Your Name <emrebilge2003@gmail.com>
2023-09-22 21:54:45 -0500

    .\.git\

--- frontend/word-guessing-game/src/GamePage/Rules.js
+++ frontend/word-guessing-game/src/GamePage/Rules.js
@@ -12,9 +12,10 @@ function Rules({ showRules, toggleRules }) {
           <h2 className="rules-title">Rules to Play 'ekreb'</h2>
           <ul className="rules-list">
             <li>Unscramble the given word to its original form.</li>
-            <li>You have a limited time to guess each word.</li>
+            <li>You have 6 rounds to guess the word.</li>
             <li>Each correct guess earns you points.</li>
             <li>Incorrect guesses won't affect your score.</li>
+            <li>You can get hints, skip a word, or reset the game.</li>
             <li>Try to achieve the highest accuracy!</li>
           </ul>
         </div>
